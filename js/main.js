04e5a3bc67765db049f247c28fa00d2781d63555
Admin <ponomarenko02041993@gmail.com>
2019-07-22 23:18:43 +0300

    new

--- js/main.js
+++ js/main.js
@@ -11,4 +11,21 @@ $(document).ready(function() {
             $(this).closest('.form-map').find('.popup').fadeIn(500);
         },500);
     });
+
+    /*-----------------counter------------------*/
+
+    $('.to-bottom').on('click', function () {
+       var $input = $(this).closest('.counter').find('input');
+       var count = parseInt($input.val()) - 1;
+       count = count < 1 ? 1 : count;
+       $input.val(count);
+       $input.change();
+       return false;
+    });
+     $('.to-top').on('click', function () {
+       var $input = $(this).closest('.counter').find('input');
+       $input.val(parseInt($input.val()) + 1);
+       $input.change();
+       return false;
+    });
 });
