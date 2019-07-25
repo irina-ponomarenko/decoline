2fcc61716b7119d6e743ce4e0599c31b37413c8b
Admin <ponomarenko02041993@gmail.com>
2019-07-25 17:32:57 +0300

    new

--- js/main.js
+++ js/main.js
@@ -59,4 +59,20 @@ $(document).ready(function() {
             }, 700);
         });
     }
+
+    /*-------------------scroll to section--------*/
+
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault(); //опустошим стандартную обработку
+        var id  = $(this).attr('href'), //заберем айдишник блока с параметром URL
+            top = $(id).offset().top; //определим высоту от начала страницы до якоря
+        $('body,html').animate({scrollTop: top}, 1500); //сделаем прокрутку за 1 с
+    });
+
+    $("#menu2").on("click","a", function (event) {
+        event.preventDefault(); //опустошим стандартную обработку
+        var id  = $(this).attr('href'), //заберем айдишник блока с параметром URL
+            top = $(id).offset().top; //определим высоту от начала страницы до якоря
+        $('body,html').animate({scrollTop: top}, 1500); //сделаем прокрутку за 1 с
+    });
 });
